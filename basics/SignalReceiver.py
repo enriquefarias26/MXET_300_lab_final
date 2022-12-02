6d5e4f0b7ccc93cc7fcdf2121bb6693f1cd9674a
Zarate0720 <112403060+Zarate0720@users.noreply.github.com>
2022-12-02 14:38:30 -0600

    Update SignalReceiver.py

--- basics/SignalReceiver.py
+++ basics/SignalReceiver.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import L1_motor as motor
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16,GPIO.IN)
@@ -6,7 +7,11 @@ GPIO.setup(16,GPIO.IN)
 int x
 def gotShot():
     if GPIO.input(16):
-        x = 1
+        motor.sendright(0.8)
+        motor.sendleft('0.8)
+        sleep(0.8)
+        motor.sendright(0)
+        motor.sendleft(0)
+        x=1
     else:
-        x = 0
-    return x
+        x=0
