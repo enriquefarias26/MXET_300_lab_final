5f15a76c07c60862d40916a9153fcf14ba9a0174
enriquefarias26 <26enriquefarias26@gmail.com>
2022-12-06 14:23:58 -0600

    Adding New Files

---
+++ basics/FinalProject_SelectDrive.py
@@ -0,0 +1,43 @@
+#creating program  to do object avoidance
+import numpy as np
+from time import sleep
+import L1_lidar as lidar
+import L1_motor as motor
+import ManualControl as Manual
+import FinalProject_ObjectAvoid2 as Auto
+
+np.set_printoptions(precision=3)                    # after math operations, don't print long values
+
+def getValid(scan):                                 # remove the rows which have invalid distances
+    dist = scan[:, 0]                               # store just first column
+    angles = scan[:, 1]                             # store just 2nd column
+    valid = np.where(dist > 0.016)                  # find values 16mm
+    myNums = dist[valid]                            # get valid distances
+    myAng = angles[valid]                           # get corresponding valid angles
+    output = np.vstack((myNums, myAng))             # recombine columns
+    n = output.T                                    # transpose the matrix
+    return n
+
+def nearest(scan):                                  # find the nearest point in the scan
+    dist = scan[:, 0]                               # store just first column
+    column_mins = np.argmin(dist, axis=0)           # get index of min values along 0th axis (columns)
+    row_index = column_mins                         # index of the smallest distance
+    vec = scan[row_index, :]                        # return the distance and angle of the nearest object in scan
+    return vec                                      # contains [r, alpha]
+
+def getNearest():                                   # combine multiple functions into one.  Call to get nearest obstacle.
+    scan = lidar.polarScan()                        # get a reading in meters and degrees
+    valids = getValid(scan)                         # remove the bad readings
+    vec = nearest(valids)                           # find the nearest
+    return vec                                      # pass the closest valid vector [m, deg]
+
+if __name__ == "__main__":
+    while True:
+        choice = input("Input 1 For Manual (When Done With Manual, ctl+c): ")
+        myVector = getNearest()
+        if choice == "1":
+            Manual.main()
+        elif choice == "2":
+            Auto.main()
+        else:
+            None
--- basics/ServoControl.py
+++ basics/ServoControl.py
@@ -2,27 +2,27 @@ from gpiozero import Servo
 from time import sleep
 
 shots = 0
-servo1 = Servo(25) #servo1 for shooting
+servo1 = Servo(24) #servo1 for shooting
 val = -1
 
-servo2 = Servo(24) #servo2 for reloading
+servo2 = Servo(25) #servo2 for reloading
 
 def shoot():
 	servo1.value = val
 	sleep (1)
 	servo1.value = val + 2
 	sleep(1)
-def reload():
+def gunreload():
 	servo2.value = val
-	sleep (1)
+	sleep (2)
 	servo2.value = val + 2
-	sleep(1)
+	sleep(2)
 
 while True:
 	shoot()
 	shots += 1
 	if (shots == 12):
-		reload()
+		gunreload()
 		shots = 0
 	sleep(1)
 
